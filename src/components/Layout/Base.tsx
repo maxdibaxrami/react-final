528eaf957ec07788f821030bc0e749b45b17317f
mahdi bahrami <mahdibahrami@mahdis-MacBook-Pro.local>
2024-11-23 19:33:48 +0300

    m

--- src/components/Layout/Base.tsx
+++ src/components/Layout/Base.tsx
@@ -35,21 +35,21 @@ const tabs = [
     {
         id:1,
         text:"Explore",
-        Icon:<ExploreIcon style={{height:"40px", width:"50px"}}/>
+        Icon:<ExploreIcon style={{height:"35px", width:"35px"}}/>
     },
     {
         id:2,
         text:"Random Chat",
-        Icon:<RandomChatIcon style={{height:"40px", width:"50px"}}/>
+        Icon:<RandomChatIcon style={{height:"35px", width:"35px"}}/>
     },
     {
         id:3,
         text:"Chat",
-        Icon:<ChatIcon style={{height:"40px", width:"50px"}}/>
+        Icon:<ChatIcon style={{height:"35px", width:"35px"}}/>
     },
     {
         id:4,
         text:"Profile",
-        Icon:<ProfileIcon style={{height:"40px", width:"50px"}}/>
+        Icon:<ProfileIcon style={{height:"35px", width:"35px"}}/>
     },
 ]
